a350c75466e8290d9f54aec515966ef19ae8cc5c
Kris Adelowo <krisphinna@gmail.com>
2021-12-06 13:06:00 +0100

    commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var dropDown = document.querySelector(".drop-down");
+var profileDropDown = document.querySelector(".profile-drop-down");
+var profileLookup = document.querySelector(".profile-lookup");
+
+
+function dropDownToggle() {
+    dropDown.classList.toggle("drop-down-height");
+}
+function profileDropDownToggle() {
+    profileDropDown.classList.toggle("profile-drop-down-height")
+}
+function profileLookupToggle() {
+    profileLookup.classList.toggle("profile-lookup-height")
+}
